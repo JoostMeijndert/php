5bf07a868c9b99724c9c6f565f5d23bee5a5be28
JoostMeijndert <54812873+JoostMeijndert@users.noreply.github.com>
2020-03-24 12:56:28 +0100

    alles naar git, H2 af

---
+++ www/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+    <a href="../hooftstukken/H01.php"><button type="button">Hoofdstuk 1</button></a>
+    <a href="../hooftstukken/H02.php"><button type="button">Hoofdstuk 2</button></a>
+    <a href="../hooftstukken/H03.php"><button type="button">Hoofdstuk 3</button></a>
+</body>
+</html>
